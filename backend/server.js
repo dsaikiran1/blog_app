71e7d59b030935ae50ec516f774e5a3e7f575fd8
dsaikiran1 <dsaikiran7787@gmail.com>
2025-03-27 23:09:18 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 // Middleware
-app.use(cors());
+//app.use(cors());
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
