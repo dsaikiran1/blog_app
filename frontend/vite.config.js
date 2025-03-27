956fcd3ff64b56d68e532195ad6069d0867bf988
dsaikiran1 <dsaikiran7787@gmail.com>
2025-03-27 22:29:40 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:5000", // Change to your backend server URL
         changeOrigin:true,
-        
+        secure:false,
       },
     },
   },
