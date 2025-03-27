98646122b12f2fc96b954315770207d5a1f476ad
dsaikiran1 <dsaikiran7787@gmail.com>
2025-03-27 15:33:32 +0530

    vite modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 3000, // Change to your preferred port
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Change to your backend server URL
+        changeOrigin:true,
+      },
+    },
+  },
   build:{
     outDir:'dist',
   },
